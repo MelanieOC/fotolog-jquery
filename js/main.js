a3a781a8696207e90bbce9198dd4f4c195d2c066
MelanieOC <melanie.ocharan@gmail.com>
2017-09-08 16:00:40 -0500

    comentarios

--- js/main.js
+++ js/main.js
@@ -21,16 +21,16 @@ const fotolog = {
 		if (localStorage.length > 0) { //cuando hay datos
 			$.each(localStorage, (key, value)=>{
 				let array = value.split(','); //se crea un array con los valores, nombre y comentario
-				$("<div>").addClass('comentario row').append(
-					`<div class='col-md-2 col-sm-4 col-xs-4 text-center'>
-						<h2 class='fa fa-user-circle fa-fw'></h2>
-					</div>
-					<div class='col-md-9 col-sm-6 col-xs-6'>
-						<h3>${array[0]}</h2>
-						<p>${array[1]}</p>
-					</div>
-					`
-				).appendTo("#ale");
+				$('#ale').append(
+						`<div class='row comentario'>
+							<div class='col-md-2 col-sm-4 col-xs-4 text-center'>
+								<h2 class='fa fa-user-circle fa-fw'></h2>
+							</div>
+							<div class='col-md-9 col-sm-6 col-xs-6'>
+								<h3>${array[0]}</h2>
+								<p>${array[1]}</p>
+							</div>
+						</div>`);
 			})
 		} else { //cuando el localStorage está vacio
 			$('#ale').append(`<div> No hay comentarios </div>`);
@@ -38,7 +38,7 @@ const fotolog = {
 	},
 
 	guardarDatos:()=>{
-		if(fotolog.datos.nombre.val() && fotolog.datos.comentario.val()){ //solo se guarda cuando se lleno los campos
+		if(fotolog.datos.nombre.val() && fotolog.datos.comentario.val()){ //solo se guarda cuando esten llenos los campos
 			localStorage.setItem(localStorage.length, `${fotolog.datos.nombre.val()}, ${fotolog.datos.comentario.val()}`);
 			fotolog.datos.nombre.val('');//se vacea los inputs
 			fotolog.datos.comentario.val('');
